783628e5893743a48d834502ded17ae2b9f749e5
Ayush <71763446+ayushclashroyale@users.noreply.github.com>
2021-12-15 03:34:16 +0530

    Create Binary_tree_inorder.java

---
+++ Tree_Questions/Binary_tree_inorder.java
@@ -0,0 +1,33 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+class Solution {
+    public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> lst = new ArrayList<Integer>();
+        inorder(root,lst);
+        return(lst);
+    }
+    public List<Integer> inorder(TreeNode root,List lst){
+        if(root==null)
+            return(new ArrayList());
+        inorder(root.left,lst);
+        lst.add(root.val);
+        inorder(root.right,lst);
+        return(lst);
+    }
+}
+
+//https://leetcode.com/problems/binary-tree-inorder-traversal/submissions/
