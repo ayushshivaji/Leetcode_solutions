477bd339f4b3fa4dbc22a045934208fdf5e6bfaa
Ayush <71763446+ayushclashroyale@users.noreply.github.com>
2021-12-23 00:39:20 +0530

    Create Reverse_a_linked_list.java

---
+++ Striver_SDE_Sheet/Reverse_a_linked_list.java
@@ -0,0 +1,26 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode reverseList(ListNode head) {
+        if(head==null||head.next==null)
+            return(head);
+        ListNode pre=null,next;
+        while(head!=null){
+            next=head.next;
+            head.next=pre;
+            pre = head;
+            head=next;
+        }
+        return(pre);
+    }
+}
+
+//https://leetcode.com/problems/reverse-linked-list/submissions/
