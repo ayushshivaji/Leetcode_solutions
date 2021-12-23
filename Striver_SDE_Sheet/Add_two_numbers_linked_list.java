c5d6f432e4cb83208e6f4605bc57777a070c8f17
Ayush <71763446+ayushclashroyale@users.noreply.github.com>
2021-12-23 11:01:45 +0530

    Create Add_two_numbers_linked_list.java

---
+++ Striver_SDE_Sheet/Add_two_numbers_linked_list.java
@@ -0,0 +1,61 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode ans = new ListNode(0);
+        ListNode head = ans;
+        int sum;int carry=0;
+        while(l1!=null&&l2!=null){
+            sum=l1.val+l2.val+carry;
+            if(sum>9){
+                carry = sum/10;
+                sum=sum%10;
+            }
+            else
+                carry=0;
+            l1=l1.next;
+            l2=l2.next;
+            ans.next=new ListNode(sum);
+            ans=ans.next;
+        }
+        while(l1!=null){
+            sum=l1.val+carry;
+            if(sum>9)
+            {
+                carry=sum/10;
+                sum=sum%10;
+            }
+            else
+                carry=0;
+            ans.next=new ListNode(sum);
+            ans=ans.next;
+            l1=l1.next;
+        }
+        while(l2!=null){
+            sum=l2.val+carry;
+            if(sum>9)
+            {
+                carry=sum/10;
+                sum=sum%10;
+            }
+            else
+                carry=0;
+            ans.next=new ListNode(sum);
+            ans=ans.next;
+            l2=l2.next;
+        }
+        if(carry!=0)
+            ans.next=new ListNode(carry);
+        return(head.next);
+    }
+}
+
+//https://leetcode.com/problems/add-two-numbers/submissions/
