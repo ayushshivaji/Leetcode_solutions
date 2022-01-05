f37d71836780a56ddb68580599bba6fc528ac782
Ayush <71763446+ayushclashroyale@users.noreply.github.com>
2022-01-06 01:26:24 +0530

    Create Search_insert_position.java

---
+++ Miscellaneous_Questions/Search_insert_position.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int searchInsert(int[] nums, int target) {
+        int start=0,end=nums.length-1;int mid=0;
+        while(start<=end){
+            mid=(start+end)/2;
+            if(nums[mid]==target)
+                return(mid);
+            else if(nums[mid]<target)
+                start=mid+1;
+            else
+                end=mid-1;
+        }
+        if(nums[mid]<target)
+            return(mid+1);
+        else
+            return(mid);
+    }
+}
+
+//https://leetcode.com/problems/search-insert-position/
