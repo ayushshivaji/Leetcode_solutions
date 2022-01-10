fe943cb0d2a715fcf63e544913e475dc066547ca
Ayush <71763446+ayushclashroyale@users.noreply.github.com>
2022-01-10 18:38:37 +0530

    Create Reverse_string.java

---
+++ Miscellaneous_Questions/Reverse_string.java
@@ -0,0 +1,12 @@
+class Solution {
+    public void reverseString(char[] s) {
+        for(int i=0,j=s.length-1;i<=j;i++,j--)
+        {
+            char temp = s[i];
+            s[i]=s[j];
+            s[j]=temp;
+        }
+    }
+}
+
+//https://leetcode.com/problems/reverse-string/submissions/
